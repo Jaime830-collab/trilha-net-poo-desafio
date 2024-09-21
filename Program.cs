1bfd6dfd1e8c3c9e15ad27d94544b44c6d22b5c7
jaime <jaimevaz75@gmail.com>
2024-09-21 10:36:15 -0300

    Primeira atualizacao

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n = new Nokia("123", "Nokia", "123", 128);
+Iphone i = new Iphone("456", "Iphone", "456", 256);
+
+n.InstalarAplicativo("WhatsApp");
+n.Ligar();
+n.ReceberLigacao();
+
+i.InstalarAplicativo("Instagram");
+i.Ligar();
+i.ReceberLigacao();
